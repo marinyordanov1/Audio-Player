534520add287f8a58f48e6f6663b129ec5af29e2
Marin Yordanov <marin.k.yordanov@gmail.com>
2025-06-12 15:02:14 +0300

    Added main source files

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,60 @@
-package PACKAGE_NAME;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
+
+
+    public static void main(String[] args) {
+        // How to play Audio File With Java (.wav .au .aiff)
+
+        String filePath = "src/Java-Wav-Music_Wav.wav";
+
+        File file = new File(filePath);
+
+        try (Scanner scanner = new Scanner(System.in);
+             AudioInputStream audioStream = AudioSystem.getAudioInputStream(file)){
+
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+
+            String response = "";
+
+            while (!response.equals("Q")) {
+                System.out.println("P = Play");
+                System.out.println("S = Stop");
+                System.out.println("R = Reset");
+                System.out.println("Q = Quit");
+                System.out.print("Enter Your Choice");
+
+                response = scanner.next().toUpperCase();
+
+                switch (response) {
+                    case "P" -> clip.start();
+                    case "S" -> clip.stop();
+                    case "R" -> clip.setMicrosecondPosition(0);
+                    case "Q" -> clip.close();
+                    default -> System.out.println("Invalid Choice");
+                }
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Could not locate file");
+        }
+        catch (UnsupportedAudioFileException e) {
+            System.out.println("Audio file not supported");
+        }
+        catch (LineUnavailableException e){
+            System.out.println("Unable to access audio resource");
+        }
+        catch (IOException e) {
+            System.out.println("Something went wrong ");
+        }
+        finally {
+            System.out.println("Bye!");
+        }
+    }
+
 }
